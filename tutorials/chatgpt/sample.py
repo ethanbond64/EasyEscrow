e6f5f153fe77a1868fd2f3aa3ce8367ac21d97a3
ethanbond64 <ethanbond39@gmail.com>
2023-03-24 21:38:49 -0600

    learning

--- tutorials/chatgpt/sample.py
+++ tutorials/chatgpt/sample.py
@@ -10,13 +10,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
         {
-            "role": "assistant",
-            "content": "The Los Angeles Dodgers won the World Series in 2020.",
+            "role": "user",
+            "content": "If I sent you the text from an escrow agreement could you identify the names of the parties and the amount in escrow, based on the text?",
         },
-        {"role": "user", "content": "Where was it played?"},
     ],
 )
 
