2ea116f173b0af63ab1b1fce21ee9e71bf06f66a
ethanbond64 <ethanbond39@gmail.com>
2023-03-24 21:58:16 -0600

    pdf to fields working

--- tutorials/chatgpt/sample.py
+++ tutorials/chatgpt/sample.py
@@ -7,14 +7,22 @@ load_dotenv()
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "user",
-            "content": "If I sent you the text from an escrow agreement could you identify the names of the parties and the amount in escrow, based on the text?",
-        },
-    ],
-)
 
-print(response)
+def getComponents(text):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are escrowGPT. Users give you the text from their escrow agreements"
+                + "and you identify the names of the parties and the amount in escrow, based on the text."
+                + "Your answers match this format: {'party1': '...', 'party2': '...', 'amount': '...'}",
+            },
+            {
+                "role": "user",
+                "content": text,
+            },
+        ],
+    )
+    print(response)
+    return response["choices"][0]["message"]["content"]
