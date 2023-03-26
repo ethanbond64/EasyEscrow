14118466a8754cdac2cca3cece018c1c3e620321
ethanbond64 <ethanbond39@gmail.com>
2023-03-26 15:51:42 -0600

    pretty input

--- frontend/src/PDFUploader.js
+++ frontend/src/PDFUploader.js
@@ -40,12 +40,15 @@ function PDFUploader() {
 
   return (
     <div className='relative m-auto flex h-2/3 p-2.5 pb-4 rounded shadow-md w-1/3 bg-slate-100'>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="file"
-          accept="application/pdf"
-          onChange={handleChange}
-        />
+      <form onSubmit={handleSubmit} className='w-full'>
+        <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-5/6 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 ">
+          <div className="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+          </div>
+          <input id="dropzone-file" type="file" accept="application/pdf" onChange={handleChange} class="hidden" />
+        </label>
         <button type="submit" class="absolute bottom-0 right-0 py-2 px-3 mb-8 mr-10 rounded text-center text-white bg-indigo-500 cursor-pointer hover:bg-indigo-400 focus:bg-indigo-600" >Upload</button>
       </form>
     </div>
