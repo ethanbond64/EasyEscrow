1ec0ea03a69091ba1c9986aca689dc9bf72aa92b
ethanbond64 <ethanbond39@gmail.com>
2023-03-26 18:01:32 -0600

    form complete

--- frontend/src/CreateEscrow.js
+++ frontend/src/CreateEscrow.js
@@ -23,6 +23,25 @@ function CreateEscrow(props) {
                             <input type="text" id="rec_addr" class="bg-gray-50 border border-gray-300 text-gray-900 mb-5 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="rXXXXXXXXXXXXXXXXXXXX" required />
                         </div>
                     </div>
+
+                    <div class="grid md:grid-cols-4 md:gap-3">
+                        <div class="relative col-span-2 z-0 w-full mb-6 group h-fit  pt-2">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Third party</label>
+                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={props.data["components"]["thirdParty"] ? props.data["components"]["thirdParty"] : "None"} disabled />
+                        </div>
+                        <div class="relative z-0 w-full mb-6 group h-fit pt-2">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Amount (USD)</label>
+                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={`$${props.data["components"]["amount"]}`} disabled />
+                        </div>
+                        <div class="relative z-0 w-full mb-6 group h-fit pt-2">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Expiration date</label>
+                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value={props.data["components"]["expiration"]} disabled />
+                        </div>
+                    </div>
+
+                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 ">Escrow condition</label>
+                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border mb-5 border-gray-300 focus:ring-blue-500 focus:border-blue-500" disabled value={props.data["components"]["condition"]}></textarea>
+
                     <button type="submit" class="absolute bottom-0 right-0 py-2 px-3 mb-8 mr-10 rounded text-center text-white bg-indigo-500 cursor-pointer hover:bg-indigo-400 focus:bg-indigo-600" >Upload</button>
 
                     {JSON.stringify(props.data)}
