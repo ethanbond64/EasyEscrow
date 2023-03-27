f9bc64d1c840a437dbf5f525320e0b620a781095
ethanbond64 <ethanbond39@gmail.com>
2023-03-27 10:55:58 -0600

    working

--- frontend/src/Complete.js
+++ frontend/src/Complete.js
@@ -11,6 +11,43 @@ function Complete(props) {
 
     const { id } = useParams();
 
+    const getData = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get('http://localhost:8000/reference/' + id);
+
+            if (response.status === 200) {
+                setCompleted(response.data["reference"]["finished"]);
+                setLoading(false);
+                console.log(response);
+            }
+        } catch (error) {
+            setLoading(false);
+            console.error('Error referencing escrow:', error);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const complete = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get('http://localhost:8000/finish/' + id);
+
+            if (response.status === 200 && response.data["success"]) {
+                setCompleted(true);
+                setLoading(false);
+                console.log(response);
+            }
+        } catch (error) {
+            setLoading(false);
+            console.error('Error completing escrow:', error);
+        }
+    };
+
+
     if (loading) {
         return <Spinner />
     }
@@ -18,7 +55,8 @@ function Complete(props) {
     return (
         <div className="relative m-auto flex h-screen">
             <div className='relative m-auto flex h-2/3 p-2.5 pb-4 rounded shadow-md w-1/3 bg-slate-100'>
-                TODO Complete
+                TODO Complete. State = {completed ? "Completed" : "Not Completed"}
+                <button type="submit" onClick={complete} className="absolute bottom-0 right-0 py-2 px-3 mb-6 mr-6 rounded text-center text-white bg-indigo-500 cursor-pointer hover:bg-indigo-400 focus:bg-indigo-600" >Complete</button>
             </div>
         </div>
     );
--- frontend/src/Validate.js
+++ frontend/src/Validate.js
@@ -5,11 +5,48 @@ import Spinner from './Spinner';
 
 function Validate(props) {
 
-    const [data, setData] = useState(null);
+    // const [data, setData] = useState(null);
     const [validated, setValidated] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const { id } = useParams();
+
+    const getData = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get('http://localhost:8000/reference/' + id);
+
+            if (response.status === 200) {
+                setValidated(response.data["reference"]["fulfilled"]);
+                setLoading(false);
+                console.log(response);
+            }
+        } catch (error) {
+            setLoading(false);
+            console.error('Error referencing escrow:', error);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const validate = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get('http://localhost:8000/validate/' + id);
+
+            if (response.status === 200 && response.data["success"]) {
+                setValidated(true);
+                setLoading(false);
+                console.log(response);
+            }
+        } catch (error) {
+            setLoading(false);
+            console.error('Error validating escrow:', error);
+        }
+    };
+
 
     if (loading) {
         return <Spinner />
@@ -18,7 +55,8 @@ function Validate(props) {
     return (
         <div className="relative m-auto flex h-screen">
             <div className='relative m-auto flex h-2/3 p-2.5 pb-4 rounded shadow-md w-1/3 bg-slate-100'>
-                TODO Validate
+                TODO Validate. State = {validated ? "Validated" : "Not validated"}
+                <button type="submit" onClick={validate} className="absolute bottom-0 right-0 py-2 px-3 mb-6 mr-6 rounded text-center text-white bg-indigo-500 cursor-pointer hover:bg-indigo-400 focus:bg-indigo-600" >Validate</button>
             </div>
         </div>
     );
